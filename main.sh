18dac863f5046acd04a3a3c1574c0101446951d2
Erikson Murrugarra <erikson.murrugarra@gmail.com>
2018-04-14 17:26:30 -0500

    Adding changes

--- main.sh
+++ main.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+set -e 
 REG_ID=$1
-FILE=$2
+FILE_URL=$2
 
 #run twistlock
 echo "running twislock"
+# twistcli ...
+
+echo "parsing twistlock output"
 
 #retrieve APIENDPOINT
-echo "getting file"
+echo "downloading file"
+mkdir -p /tmp/folder_${REG_ID}
+ZIP_FILE=/tmp/folder_${REG_ID}/${REG_ID}_file.zip
+curl -k -o $ZIP_FILE $FILE_URL
 
 #unzip json 
 echo "unziping file"
+tar xvf $ZIP_FILE /tmp/folder_${REG_ID}
 
 echo "parsing file"
+FILE=/tmp/folder_${REG_ID}/data.json
 python /opt/app/parse.py $REG_ID $FILE
